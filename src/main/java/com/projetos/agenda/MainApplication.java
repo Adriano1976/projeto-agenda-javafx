76a60f3cea10a689e5bb5853bc0d6bd0e1010c8f
Adriano Santos <adriradiologista@hotmail.com.br>
2022-08-17 08:28:39 -0300

    Criando as pastas icons e image. Colocando icon na tela principal.

--- src/main/java/com/projetos/agenda/MainApplication.java
+++ src/main/java/com/projetos/agenda/MainApplication.java
@@ -3,15 +3,20 @@ package com.projetos.agenda;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("TelaPrincipal.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader root = new FXMLLoader(MainApplication.class.getResource("TelaPrincipal.fxml"));
+        Image applicationIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("icons/iconeAplicacao..png")));
+
+        Scene scene = new Scene(root.load(), 320, 240);
+        stage.getIcons().add(applicationIcon);
         stage.setTitle("Agenda de Contatos");
         stage.setMaximized(true);
         stage.setScene(scene);
