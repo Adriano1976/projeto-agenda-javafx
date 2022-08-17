8956abd5f2380bd512c4a160b369e05f3c4e26cd
Adriano Santos <adriradiologista@hotmail.com.br>
2022-08-17 16:29:48 -0300

    Criando o formulário de Usuário.

---
+++ src/main/java/com/projetos/agenda/controller/UsuarioController.java
@@ -0,0 +1,61 @@
+package com.projetos.agenda.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UsuarioController implements Initializable {
+    @FXML
+    public TextField tfId;
+    @FXML
+    public TextField tfDescricao;
+    @FXML
+    public Button btnNovo;
+    @FXML
+    public Button btnSalvar;
+    @FXML
+    public Button btnExcluir;
+    @FXML
+    public TextField tfPesquisa;
+    @FXML
+    public TableView tableView;
+    @FXML
+    public PasswordField pfSenha;
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     *                  the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void incluirResgistro(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void salvarResgistro(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void excluirResgistro(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void pesquisar(ActionEvent actionEvent) {
+    }
+}
