7bac076b0cc099a5f92ec17333bc24e87394e0d5
Adriano Santos <adriradiologista@hotmail.com.br>
2022-08-17 13:34:32 -0300

    Criando as classes no pacote Model

---
+++ src/main/java/com/projetos/agenda/model/TipoContato.java
@@ -0,0 +1,22 @@
+package com.projetos.agenda.model;
+
+public class TipoContato {
+    private Long id;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
